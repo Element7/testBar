dcbac6e8e627fd88f94ac710e8454c35cc9003de
Kacper <kjabcon@gmail.com>
2019-09-05 21:42:59 +0200

    Update widget.js
    Cleaning unnecessary code.

--- widget.js
+++ widget.js
@@ -3,11 +3,13 @@ const closure = document.createElement('div');
 const wrapper = document.querySelector('.wrapper');
 const displayBtn = document.querySelector('.displayBtn')
 function Bar(obj) {
+    
     if (!obj.myCustom) {
         obj.myCustom = {
             animate: true,
         }
     }
+    
     closure.innerText = "X";
     closure.classList.add('closure')
     bar.innerHTML = obj.message + `<span class="bar__widgetBtn"}> Get widgets </span>`;
@@ -47,10 +49,7 @@ function Bar(obj) {
     widgetBtn.addEventListener('click', this.handleWidget)
 }
 
-
-
 Bar.prototype.handleBtn = function () {
-    const bar = document.querySelector('.bar');
     const widgetBtn = document.querySelector('.bar__widgetBtn');
     bar.classList.toggle('bar__element--visible');
     bar.classList.toggle('bar__element--animation');
@@ -58,7 +57,6 @@ Bar.prototype.handleBtn = function () {
 }
 
 Bar.prototype.handleClosureBtn = function () {
-    const bar = document.querySelector('.bar');
     const widgetBtn = document.querySelector('.bar__widgetBtn');
     widgetBtn.classList.remove('bar__widgetBtn--animation');
     bar.classList.remove('bar__element--visible', 'bar__element--animation');
