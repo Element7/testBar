fe3dcea3230060db842e4c6f8cec567df24b43ad
kacper <kjabcon@gmail.com>
2019-09-05 21:26:22 +0200

    Customizable method complete. README file update.

--- widget.js
+++ widget.js
@@ -1,10 +1,16 @@
+const bar = document.createElement('div');
+const closure = document.createElement('div');
+const wrapper = document.querySelector('.wrapper');
+const displayBtn = document.querySelector('.displayBtn')
 function Bar(obj) {
-    const bar = document.createElement('div');
-    const closure = document.createElement('div');
-    const wrapper = document.querySelector('.wrapper');
-    const displayBtn = document.querySelector('.displayBtn')
+    if (!obj.myCustom) {
+        obj.myCustom = {
+            animate: true,
+        }
+    }
     closure.innerText = "X";
-    bar.innerHTML = obj.message + `<button class="bar__widgetBtn bar__widgetBtn--transform"}> Get widgets </button>`;
+    closure.classList.add('closure')
+    bar.innerHTML = obj.message + `<span class="bar__widgetBtn"}> Get widgets </span>`;
     bar.classList.add('bar');
     closure.classList.add('bar__closureBtn');
     wrapper.appendChild(bar);
@@ -14,15 +20,31 @@ function Bar(obj) {
         return alert('Provided type of details is incorrect.')
     } else {
         if (obj.position === 'top') {
-            bar.classList.add('bar__element--top-position', 'bar__top-element--transform')
+            bar.classList.add('bar__element--top-position')
         } else if (obj.position === 'bottom') {
             bar.classList.add('bar__element--bottom-position', 'bar__bottom-element--transform')
         }
     }
 
+    const widgetBtn = document.querySelector('.bar__widgetBtn')
 
+    if (obj.myCustom.animate === true) {
+        bar.classList.add('bar__top-element--transform')
+        widgetBtn.classList.add('bar__widgetBtn--transform')
+    }
+
+    this.handleWidget = function () {
+        if (!obj.myCustom.background) {
+            alert('hello world');
+        }
+        else if (obj.myCustom.animate) {
+            bar.style.color = obj.myCustom.color;
+            bar.style.backgroundColor = obj.myCustom.background;
+        }
+    }
     closure.addEventListener('click', this.handleClosureBtn)
     displayBtn.addEventListener('click', this.handleBtn)
+    widgetBtn.addEventListener('click', this.handleWidget)
 }
 
 
@@ -41,5 +63,4 @@ Bar.prototype.handleClosureBtn = function () {
     widgetBtn.classList.remove('bar__widgetBtn--animation');
     bar.classList.remove('bar__element--visible', 'bar__element--animation');
 }
-
 
